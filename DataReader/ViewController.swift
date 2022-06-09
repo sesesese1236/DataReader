9fe9a2470ba362742b3f51c1b0425deebb469f4d
WENDRA RIANTO <2020jn23@iMac202002noiMac.local>
2022-06-09 16:11:59 +0900

    習得したでーたを分解して使用できるようにしていく

--- DataReader/ViewController.swift
+++ DataReader/ViewController.swift
@@ -7,20 +7,21 @@
 
 import UIKit
 
-class shiftData{
+struct ShiftData{
     var Date=""
     var Earn=0
-    var WorkTime=time()
-    var TimeLeft=time()
+    var WorkTime=Time()
+    var TimeLeft=Time()
 }
-class time{
+struct Time{
     var Hour=0
     var Minute=0
 }
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var ListViewer: UITableView!
-    var shiftDataList:[shiftData] = []
+    var shiftDataList:[ShiftData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         ListViewer.delegate = self
@@ -29,7 +30,33 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let url = URL(string:"http://192.168.1.217:3010/sendData")!
         
         let task = URLSession.shared.dataTask(with: url){(data,response,error) in guard let data = data else{return}
-            print(String(data:data, encoding: .utf8)!)
+//            print(String(data:data, encoding: .utf8)!)
+            do{
+                let jsonData = try JSONSerialization.jsonObject(with:data,options:JSONSerialization.ReadingOptions.allowFragments)
+                for dt_base in jsonData as! [Any]{
+                    let dt = dt_base as! [String:Any]
+                    let wt = dt["WorkTime"] as! [String:Any]
+                    let tl = dt["TimeLeft"] as! [String:Any]
+                    let shift = ShiftData(
+                        Date: dt["Date"] as! String,
+                        Earn: dt["Earn"] as! Int,
+                        WorkTime: Time(
+                            Hour: wt["hour"] as! Int,
+                            Minute: wt["minute"] as! Int),
+                        TimeLeft: Time(
+                            Hour: tl["hour"] as! Int,
+                            Minute: tl["minute"] as! Int))
+                    self.shiftDataList.append(shift)
+                }
+                print(self.shiftDataList.count)
+//                self.ListViewer.reloadData()
+            }catch{
+                print("error")
+            }
+//            for dt in data{
+//                print(String(dt))
+//            }
+            
         }
         task.resume()
         // Do any additional setup after loading the view.
@@ -37,12 +64,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         var count = 0
+        count = shiftDataList.count
         return count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell =
         tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Date \(shiftDataList[indexPath.row].Date),TimeLeft:\(shiftDataList[indexPath.row].TimeLeft)"
         return cell
     }
     func tableView(_ tableView:UITableView, didSelectRowAt indexPath: IndexPath){
